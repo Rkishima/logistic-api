f6c90c8824c92133e063468a9cb623666d6e62a8
Ricardo Tadashi de Souza Kishima <ricardokishima@hotmail.com>
2021-05-24 02:01:04 -0300

    Bean Validation

--- src/main/java/br/com/rkishima/controller/ClienteController.java
+++ src/main/java/br/com/rkishima/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @AllArgsConstructor
@@ -33,12 +34,12 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente){
+    public Cliente create(@Valid @RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Cliente> update (@PathVariable Long id, @RequestBody Cliente cliente){
+    public ResponseEntity<Cliente> update (@Valid @PathVariable Long id, @RequestBody Cliente cliente){
         if (!clienteRepository.existsById(id)){
             return ResponseEntity.notFound().build();
         }
--- src/main/java/br/com/rkishima/domain/model/Cliente.java
+++ src/main/java/br/com/rkishima/domain/model/Cliente.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @EqualsAndHashCode
 @Data
@@ -15,8 +18,17 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 60)
     private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String phone;
 
 
