53f8d6d43dadc355e3f1fc05c8b3447320f1ad78
RKishima <ricardokishima@hotmail.com>
2022-11-09 22:00:23 -0300

    CRUD implementation

--- logistic-api/src/main/java/com/rkorp/logisticapi/api/controller/ClienteController.java
+++ logistic-api/src/main/java/com/rkorp/logisticapi/api/controller/ClienteController.java
@@ -1,24 +1,58 @@
 package com.rkorp.logisticapi.api.controller;
 
-import com.rkorp.logisticapi.com.rkorp.logisticapi.domain.model.Cliente;
+import com.rkorp.logisticapi.domain.model.Cliente;
+import com.rkorp.logisticapi.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
     @Autowired
+    private ClienteRepository clienteRepository;
 
-    @GetMapping("/listar")
-    public String listar(){
-        return "teste";
+
+    @GetMapping()
+    public List<Cliente> findAll() {
+        return clienteRepository.findAll();
     }
 
-    @PostMapping("/cliente")
-    public Cliente saveClient(Cliente cliente){
-        return cliente;
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> findById(Long clienteId) {
+        return clienteRepository.findById(clienteId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente saveClient(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateClient(Long id, @RequestBody Cliente cliente) {
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        cliente.setId(id);
+        cliente = clienteRepository.save(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteClient(@PathVariable Long id){
+        if (!clienteRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        clienteRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- logistic-api/src/main/java/com/rkorp/logisticapi/domain/model/Cliente.java
+++ logistic-api/src/main/java/com/rkorp/logisticapi/domain/model/Cliente.java
@@ -1,8 +1,26 @@
-package com.rkorp.logisticapi.com.rkorp.logisticapi.domain.model;
+package com.rkorp.logisticapi.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cliente {
+
+    @Id
+    @EqualsAndHashCode.Exclude
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String nome;
+
+    @Column
     private String email;
+
+    @Column
     private String telefone;
 }
